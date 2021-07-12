b45579c0c0ae4a1e422ceae52752ae1f7159b9bf
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-07-12 10:06:51 -0300

    Update main.js
    004

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,7 @@
-alert("Meu primeiro JS")
+var nome = "HEDER SILVA"
+var idade = 31
+var frase = "Qual sentido da vida?"
+alert(nome + "tem" + idade + "anos")
+console.log(nome)
+console.log(idade)
+console.log(frase.toUpperCase)
