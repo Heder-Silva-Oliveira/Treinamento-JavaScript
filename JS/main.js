576f7ee88b2dbe6711eb0534156f35bf9b57118a
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-07-12 10:24:58 -0300

    Update main.js
    005

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,17 @@
-var nome = "HEDER SILVA"
+/*var nome = "HEDER SILVA"
 var idade = 31
 var frase = "Qual sentido da vida?"
 alert(nome + "tem" + idade + "anos")
 console.log(nome)
 console.log(idade)
 console.log(frase.toUpperCase)
+
+var lista = ["Maça", "Pera", "Laranja"]
+lista.push("Uva")
+lista.pop();
+console.log(lista);
+console.log(lista.toString())
+console.log(lista.join(" - "))*/
+var frutas = [{nome:"Maça", cor:"Vermelho"}, {nome:"Uva", cor:"Roxa"}]
+console.log(frutas)
+alert(frutas[1].nome)
