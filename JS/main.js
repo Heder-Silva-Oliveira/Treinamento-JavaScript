cf0f70b4be32036dc5d9dbf08d1f5be52f5e7039
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-07-13 08:37:08 -0300

    Update main.js
    006

--- JS/main.js
+++ JS/main.js
@@ -11,7 +11,21 @@ lista.push("Uva")
 lista.pop();
 console.log(lista);
 console.log(lista.toString())
-console.log(lista.join(" - "))*/
+console.log(lista.join(" - "))
 var frutas = [{nome:"Maça", cor:"Vermelho"}, {nome:"Uva", cor:"Roxa"}]
 console.log(frutas)
 alert(frutas[1].nome)
+var nome = prompt("Qual seu nome? ");
+var idade = prompt("Qual sua idade? ");
+if (idade >= 18){
+    alert(nome + " é maior de idade");
+}else{
+    alert(nome + " é menor e idade");
+};*/
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+}
+
+function redirecionar(){
+    
+}
